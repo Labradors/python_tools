5bb89fe5d9c8ade785e4012dfeae1b0a294b3df9
Labradors <jiangtao103cp@163.com>
2018-04-25 00:10:27 +0800

    fix

--- extract_images.py
+++ extract_images.py
@@ -26,7 +26,6 @@ class VectorCompare:
 
 def buildvector(im):
     d1 = {}
-
     count = 0
     for i in im.getdata():
         d1[count] = i
@@ -34,71 +33,76 @@ def buildvector(im):
 
     return d1
 
-v = VectorCompare()
 
 
-iconset = ['0','1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+def extract_image(image):
+    result = ''
+    v = VectorCompare()
+    iconset = ['0','1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    imageset = []
+    for letter in iconset:
+        for img in os.listdir('./python_captcha/iconset/%s/'%(letter)):
+            temp = []
+            if img != "Thumbs.db" and img != ".DS_Store": # windows check...
+                temp.append(buildvector(Image.open("./python_captcha/iconset/%s/%s"%(letter,img))))
+            imageset.append({letter:temp})
+    im = image
+    im2 = Image.new("P",im.size,255)
+    im.convert("P")
+    temp = {}
+
+    for x in range(im.size[1]):
+        for y in range(im.size[0]):
+            pix = im.getpixel((y,x))
+            temp[pix] = pix
+            if pix == 220 or pix == 227: # these are the numbers to get
+                im2.putpixel((y,x),0)
+
+    inletter = False
+    foundletter=False
+    start = 0
+    end = 0
+
+    letters = []
+
+    for y in range(im2.size[0]): # slice across
+        for x in range(im2.size[1]): # slice down
+            pix = im2.getpixel((y,x))
+            if pix != 255:
+                inletter = True
+
+        if foundletter == False and inletter == True:
+            foundletter = True
+            start = y
+
+        if foundletter == True and inletter == False:
+            foundletter = False
+            end = y
+            letters.append((start,end))
 
 
-imageset = []
+        inletter=False
 
-for letter in iconset:
-    for img in os.listdir('./python_captcha/iconset/%s/'%(letter)):
-        temp = []
-        if img != "Thumbs.db" and img != ".DS_Store": # windows check...
-            temp.append(buildvector(Image.open("./python_captcha/iconset/%s/%s"%(letter,img))))
-        imageset.append({letter:temp})
+    count = 0
+    for letter in letters:
+        m = hashlib.md5()
+        im3 = im2.crop(( letter[0] , 0, letter[1],im2.size[1] ))
 
+        guess = []
 
-im = Image.open("./python_captcha/captcha.gif")
-im2 = Image.new("P",im.size,255)
-im.convert("P")
-temp = {}
+        for image in imageset:
+            for x,y in image.items():
+                if len(y) != 0:
+                    guess.append( (v.relation(y[0],buildvector(im3)),x) )
 
-for x in range(im.size[1]):
-    for y in range(im.size[0]):
-        pix = im.getpixel((y,x))
-        temp[pix] = pix
-        if pix == 220 or pix == 227: # these are the numbers to get
-            im2.putpixel((y,x),0)
+        guess.sort(reverse=True)
+        result+=guess[0][1]
+        count += 1
+    if len(result) > 0:
+        return result
+    else :
+        extract_image(image)
 
-inletter = False
-foundletter=False
-start = 0
-end = 0
-
-letters = []
-
-for y in range(im2.size[0]): # slice across
-    for x in range(im2.size[1]): # slice down
-        pix = im2.getpixel((y,x))
-        if pix != 255:
-            inletter = True
-
-    if foundletter == False and inletter == True:
-        foundletter = True
-        start = y
-
-    if foundletter == True and inletter == False:
-        foundletter = False
-        end = y
-        letters.append((start,end))
-
-
-    inletter=False
-
-count = 0
-for letter in letters:
-    m = hashlib.md5()
-    im3 = im2.crop(( letter[0] , 0, letter[1],im2.size[1] ))
-
-    guess = []
-
-    for image in imageset:
-        for x,y in image.items():
-            if len(y) != 0:
-                guess.append( ( v.relation(y[0],buildvector(im3)),x) )
-
-    guess.sort(reverse=True)
-    print ("",guess[0])
-    count += 1
+if __name__ == '__main__':
+    im = Image.open("captcha.gif")
+    extract_image(im)
--- simulate_login.py
+++ simulate_login.py
@@ -1,6 +1,10 @@
 import requests
 from selenium import webdriver
+from PIL import Image
 import json
+import os
+import extract_images
+import time
 
 
 
@@ -23,16 +27,55 @@ def login():
             wd = webdriver.Chrome() 
             loginUrl = 'https://passport.jd.com/new/login.aspx' 
             wd.get(loginUrl)
+            wd.find_element_by_xpath('//*[@id="content"]/div[2]/div[1]/div/div[3]').click()
             wd.find_element_by_id('loginname').send_keys(login_name)
             wd.find_element_by_id('nloginpwd').send_keys(login_password)
             wd.find_element_by_id('loginsubmit').click()
-            wd.get('https://item.jd.com/6171814.html')
-            # 点击预约商品
-            wd.find_element_by_id('btn-reservation').click()
-            # req = requests.Session() #构建Session
-            # cookies = wd.get_cookies() #导出cookie
-            # for cookie in cookies:
-            #     req.cookies.set(cookie['name'],cookie['value']) #转换cookies
-            # test = req.get('待测试的链接')
+            time.sleep(3)
+            wd.get_screenshot_as_file('jd_login.png')
+             # 图片地址
+            # vert = wd.find_element_by_id('JD_Verification1').get_attribute('src')
+            # print(vert)
+            # 下载图片
+            # save_img(vert)
+            screen_img()
+            # 解析图片文字并且填如
+            im = Image.open("pic.gif")
+            verification_code = extract_images.extract_image(im)
+            print("当前验证码："+verification_code)
+            if verification_code is not None:
+                wd.find_element_by_id('authcode').send_keys(verification_code)
+                wd.find_element_by_id('loginsubmit').click()
+                wd.get('https://item.jd.com/6171814.html')
+                # 点击预约商品
+                wd.find_element_by_id('btn-reservation').click()
+                # req = requests.Session() #构建Session
+                # cookies = wd.get_cookies() #导出cookie
+                # for cookie in cookies:
+                #     req.cookies.set(cookie['name'],cookie['value']) #转换cookies
+                # test = req.get('待测试的链接')
+            else:
+                print('验证码识别错误...')
         finally:
             wd.close()
+
+def save_img(url):
+    img = requests.get(url)
+    with open('pic.png', 'wb') as file:
+        file.write(img.content)
+
+
+def screen_img():
+    im = Image.open("jd_login.png")
+    img_size = im.size
+    print("图片宽度和高度分别是{}".format(img_size))
+    # 截取图片中一块宽和高都是250的
+    x = 1890
+    y = 754
+    w = 139
+    h = 66
+    region = im.crop((x, y, x+w, y+h))
+    region.save("pic.gif")
+
+if __name__ == '__main__':
+    login()
